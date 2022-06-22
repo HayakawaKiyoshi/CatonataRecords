1f99589aad13d60cdf0d22af1f77700204038e61
hokazono <hokazono@EGRID-PC34.egrid.co.jp>
2022-06-22 14:55:15 +0900

    新規投稿
    外薗 2022/06/22
    新規作成

---
+++ CatonataRecords/src/main/java/com/catonata/CatonataRecordesApplication.java
@@ -0,0 +1,13 @@
+package com.catonata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatonataRecordesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatonataRecordesApplication.class, args);
+	}
+
+}
---
+++ CatonataRecords/src/test/java/com/catonata/CatonataRecordesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catonata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatonataRecordesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
