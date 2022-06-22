80cd3c6e9c9959f049a88f02917d948cc8864e51
伊藤 馨 < @192.168.100.7>
2022-06-22 15:18:37 +0900

    CatonataRecordsApplication
    伊藤 2022/6/22
    コメント追加

--- CatonataRecords/src/main/java/com/catonata/CatonataRecordesApplication.java
+++ CatonataRecords/src/main/java/com/catonata/CatonataRecordesApplication.java
@@ -3,6 +3,7 @@ package com.catonata;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//伊藤です。(練習です。)
 @SpringBootApplication
 public class CatonataRecordesApplication {
 
