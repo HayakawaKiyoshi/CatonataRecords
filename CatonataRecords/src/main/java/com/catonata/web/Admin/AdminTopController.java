1b5b2aa11201fe77aa81d4c2615164235e01dec0
伊藤 馨 < @192.168.100.7>
2022-06-23 10:49:22 +0900

    AdminTopController
    伊藤 6/23
    中身更新

--- CatonataRecords/src/main/java/com/catonata/web/Admin/AdminTopController.java
+++ CatonataRecords/src/main/java/com/catonata/web/Admin/AdminTopController.java
@@ -1,5 +1,19 @@
 package com.catonata.web.Admin;
 
-public class AdminTopController {
+import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/Admin")
+public class AdminTopController {
+	@Autowired
+	HttpSession session;
+
+	@RequestMapping(path="/top")
+	private String admintop () {
+		return "admin/mypage/UserTop";
+	}
 }
