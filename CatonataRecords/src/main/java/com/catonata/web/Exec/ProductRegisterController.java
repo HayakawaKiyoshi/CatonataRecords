6978fd0f8b3ccb89a3545ea1ebb4efc8c2647da3
akutsu <akutsu@EGRID-PC30.egrid.co.jp>
2022-06-23 14:34:23 +0900

    productregisterControllerの変更
    阿久津 2022/6/23

--- CatonataRecords/src/main/java/com/catonata/web/Exec/ProductRegisterController.java
+++ CatonataRecords/src/main/java/com/catonata/web/Exec/ProductRegisterController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.catonata.dao.ExecDao;
 import com.catonata.validation.ProductForm;
+
 
 
 @Controller
@@ -20,7 +22,7 @@ public class ProductRegisterController {
 	HttpSession session;
 
 	@RequestMapping(path = "/register", method = RequestMethod.POST)
-	public ModelAndView index2(@Validated() ProductForm form,BindingResult result, ModelAndView mav) {
+	public ModelAndView index2(@Validated ProductForm form,BindingResult result, ModelAndView mav) {
 //		InsertForm user = (InsertForm) session.getAttribute("user");
 //		mav.addObject("user", user);
 
@@ -32,11 +34,48 @@ public class ProductRegisterController {
 
 			//入力フォームをセッションに保存
 			session.setAttribute("proForm", form);
-			mav.setViewName("insert/insertCheck");
+			mav.setViewName("exec/register/ProductCheck");
 		}
 
 		return mav;
 
+	}
+
+	@RequestMapping(path = "/register/execute", method = RequestMethod.POST)
+	public ModelAndView insert(ModelAndView mav) {
+		mav.setViewName("exec/register/ProductComplete");
+//		InsertForm user = (InsertForm) session.getAttribute("user");
+//		mav.addObject("user", user);
+
+		//入力内容をセッションから取得
+		ProductForm proForm = (ProductForm) session.getAttribute("proForm");
+
+		//登録を実行するDaoを呼び出す
+		ExecDao.productRegister(proForm);
+
+
+		//セッションインスタンスの削除
+		session.removeAttribute("proForm");
+
+		return mav;
+	}
+
+	/**
+	 * 入力画面に戻るメソッド
+	 * @param mav
+	 * @return
+	 */
+	@RequestMapping(path = "/register/back")
+	public ModelAndView back(ModelAndView mav) {
+//		InsertForm user = (InsertForm) session.getAttribute("user");
+//		mav.addObject("user", user);
+		mav.setViewName("exec/register/ProductRegister");
+
+		 //入力した内容を取得
+		 ProductForm form = (ProductForm) session.getAttribute("proForm");
+		 mav.addObject("productForm", form);
+
+		return mav;
 	}
 
 
