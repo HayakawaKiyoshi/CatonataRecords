752ccdb9cef0d682689133a169d91018c8eb3663
伊藤 馨 < @192.168.100.7>
2022-06-23 12:32:13 +0900

    AdminUpdateUserController
    伊藤 6/23
    中身更新

--- CatonataRecords/src/main/java/com/catonata/web/Admin/AdminUpdateUserController.java
+++ CatonataRecords/src/main/java/com/catonata/web/Admin/AdminUpdateUserController.java
@@ -1,5 +1,25 @@
 package com.catonata.web.Admin;
 
-public class AdminUpdateUserController {
+import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.catonata.bean.UserInformationBean;
+import com.catonata.validation.UserInformationForm;
+
+@Controller
+@RequestMapping("/AdminUpdate")
+public class AdminUpdateUserController {
+	@Autowired
+	HttpSession session;
+
+	@RequestMapping("")
+	private String registerUser (@ModelAttribute UserInformationForm uif) {
+		UserInformationBean LoginUser = (UserInformationBean)session.getAttribute("LoginUser");
+		session.setAttribute("LoginUser", LoginUser);
+		return "admin/update/GeneralUpdate";
+	}
 }
