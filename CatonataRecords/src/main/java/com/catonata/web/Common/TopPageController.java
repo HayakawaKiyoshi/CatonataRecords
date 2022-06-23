9a865ced3cab8fbff70a4e14e80a84225d5e2603
伊藤 馨 < @192.168.100.7>
2022-06-23 10:34:01 +0900

    TopPageController
    伊藤 6/23
    中身更新
    一時中断状態です。

--- CatonataRecords/src/main/java/com/catonata/web/Common/TopPageController.java
+++ CatonataRecords/src/main/java/com/catonata/web/Common/TopPageController.java
@@ -1,5 +1,21 @@
 package com.catonata.web.Common;
 
-public class TopPageController {
+import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TopPageController {
+	@Autowired
+	HttpSession session;
+
+	@RequestMapping(path="/TopPage")
+	private String toppage () {
+		//表示するものは商品全件表示
+//		ArrayList<ProductBean> ap = Common.allProduct(ap);
+//		session.setAttribute("allproduct", ap);
+		return "sitetop/SiteTop";
+	}
 }
