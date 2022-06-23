0ad5a6bd5913abe364e1b54661ef3d382275e2b7
伊藤 馨 < @192.168.100.7>
2022-06-23 11:21:58 +0900

    test

--- CatonataRecords/src/main/java/com/catonata/web/Admin/AdminTopController.java
+++ CatonataRecords/src/main/java/com/catonata/web/Admin/AdminTopController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.catonata.bean.UserInformationBean;
+
 @Controller
 @RequestMapping("/Admin")
 public class AdminTopController {
@@ -20,8 +22,8 @@ public class AdminTopController {
 		//経営者のユーザー情報一覧取得、セッション保存
 
 		//ログインユーザーの情報ゲット→保存
-//		UserInformationBean LoginUser = (UserInformationBean)session.getAttribute("LoginUser");
-//		session.setAttribute("LoginUser", LoginUser);
+		UserInformationBean LoginUser = (UserInformationBean)session.getAttribute("LoginUser");
+		session.setAttribute("LoginUser", LoginUser);
 		return "admin/mypage/UserTop";
 	}
 }
