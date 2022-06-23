245a4a7058777e99ff35637de254007c0b6eb322
akutsu <akutsu@EGRID-PC30.egrid.co.jp>
2022-06-23 10:48:58 +0900

    テスト

--- CatonataRecords/src/main/java/com/catonata/web/Common/LoginLogoutController.java
+++ CatonataRecords/src/main/java/com/catonata/web/Common/LoginLogoutController.java
@@ -1,34 +1,43 @@
 package com.catonata.web.Common;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.catonata.validation.LoginForm;
 
 @Controller
 public class LoginLogoutController {
 
-//	@Autowired
-//	HttpSession session;
-//
-//	@RequestMapping(path = "/login", method = RequestMethod.GET)
-//	public String index(LoginForm form) {
-//		return "login/login";
-//	}
-//
-//	@RequestMapping(path = "/login", method = RequestMethod.POST)
-//	public ModelAndView index2(@Validated LoginForm form, BindingResult bindingResult,
-//			ModelAndView mav) {
-//
-//
-//			//バリデーションエラ-
-//			if (bindingResult.hasErrors()) {
-//				mav.setViewName("login/login");
-//
-//			} else {
-//
-//				//id検索するDAOを呼び出す
-//				InsertForm emp = EmployeeDao.find(form.getId());
-//
-//				boolean isLogin = (emp != null && form.getId().equals(emp.getId()))
-//						&& form.getPass().equals(emp.getPass());
+	@Autowired
+	HttpSession session;
+
+	@RequestMapping(path = "/login", method = RequestMethod.GET)
+	public String index(LoginForm form) {
+		return "login/login";
+	}
+
+	@RequestMapping(path = "/login", method = RequestMethod.POST)
+	public ModelAndView index2(@Validated LoginForm form, BindingResult bindingResult,
+			ModelAndView mav) {
+
+
+			//バリデーションエラ-
+			if (bindingResult.hasErrors()) {
+				mav.setViewName("login/login");
+
+			} else {
+
+				//id検索するDAOを呼び出す
+
+
+//				boolean isLogin = ();
 //				if (isLogin) {
 //					//ログイン情報をセッションに保存
 //					session.setAttribute("user", emp);
@@ -37,12 +46,12 @@ public class LoginLogoutController {
 //					mav.setViewName("login/login");
 //					mav.addObject("msg", "ログインに失敗しました");
 //				}
-//
-//			}
-//
-//
-//		return mav;
-//
-//	}
-//
+
+			}
+
+
+		return mav;
+
+	}
+
 }
