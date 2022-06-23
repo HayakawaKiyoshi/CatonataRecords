bcce60455f64f09f76dbb80c6a68bd20aa16c400
akutsu <akutsu@EGRID-PC30.egrid.co.jp>
2022-06-23 14:44:06 +0900

    ExecDaoの変更
    阿久津 2022/6/23

--- CatonataRecords/src/main/java/com/catonata/dao/ExecDao.java
+++ CatonataRecords/src/main/java/com/catonata/dao/ExecDao.java
@@ -8,7 +8,8 @@ import com.catonata.validation.ProductForm;
 
 
 /**
- * 1.商品登録
+ * 1.productRegister()：商品登録
+ * 2.productUpdate()：商品更新
  * @author akutsu
  *
  */
@@ -42,6 +43,39 @@ public class ExecDao {
 
 		}finally {
 			manager.close(conn);
+		}
+	}
+
+	public void productUpdate(ProductForm form) {
+		Connection conn = null;
+		PreparedStatement ps = null;
+		DBManager manager = new DBManager();
+
+		try {
+			conn = manager.getConn();
+
+			final String SQL = "UPDATE PRODUCT_TABLE \r\n"
+					+ "SET ARTIST = ?, MEDIA = ?,PRICE = ?,\r\n"
+					+ "RELEASE_DATE= ?, LABEL= ?,SOLD = ?,STOCK =? \r\n"
+					+ "WHERE PROD_NAME = ?";
+			ps = conn.prepareStatement(SQL);
+			//引数を?にバインド
+			ps.setString(1, form.getArtist());
+			ps.setString(2, form.getMedia());
+			ps.setString(3, form.getPrice());
+			ps.setString(4, form.getRelease_date());
+			ps.setString(5, form.getLabel());
+			ps.setString(6, form.getSold());
+			ps.setString(7, form.getStock());
+			ps.setString(8, form.getPro_name());
+			ps.executeUpdate();
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+
 		}
 	}
 
