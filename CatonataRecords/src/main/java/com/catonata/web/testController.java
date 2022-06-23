7a93557501cf33636c361921d6f516612ac13115
thayakawa <thayakawa@EGRID-PC31.egrid.co.jp>
2022-06-23 10:14:54 +0900

    web/testController
    hayakawa 6/23
    LayoutTestを起動できるよう、コントローラー内を編集しました。

--- CatonataRecords/src/main/java/com/catonata/web/testController.java
+++ CatonataRecords/src/main/java/com/catonata/web/testController.java
@@ -1,9 +1,18 @@
 package com.catonata.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/Catonata")
 public class testController {
 
+//	http://localhost:10000/Catonata/leyout/test
+	@RequestMapping("/layout/test")
+	public ModelAndView layoutTest(ModelAndView mav){
+		mav.setViewName("/layout/LayoutTest");
+		return mav;
+	}
 }
 
