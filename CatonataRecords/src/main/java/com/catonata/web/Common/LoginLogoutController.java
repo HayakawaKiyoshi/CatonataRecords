65db795adfe9334e14f5c8c71f42a426165522b2
akutsu <akutsu@EGRID-PC30.egrid.co.jp>
2022-06-23 10:40:58 +0900

    LoginLogoutControllerの変更
    阿久津 2022/6/23

--- CatonataRecords/src/main/java/com/catonata/web/Common/LoginLogoutController.java
+++ CatonataRecords/src/main/java/com/catonata/web/Common/LoginLogoutController.java
@@ -1,2 +1,46 @@
 package com.catonata.web.Common;
 
+//@Controller
+//public class LoginLogoutController {
+
+//	@Autowired
+//	HttpSession session;
+//
+//	@RequestMapping(path = "/login", method = RequestMethod.GET)
+//	public String index(LoginForm form) {
+//		return "login/login";
+//	}
+//
+//	@RequestMapping(path = "/login", method = RequestMethod.POST)
+//	public ModelAndView index2(@Validated LoginForm form, BindingResult bindingResult,
+//			ModelAndView mav) {
+//
+//
+//			//バリデーションエラーの確認
+//			if (bindingResult.hasErrors()) {
+//				mav.setViewName("login/login");
+//
+//			} else {
+//
+//				//id検索するDAOを呼び出す
+//				InsertForm emp = EmployeeDao.find(form.getId());
+//
+//				boolean isLogin = (emp != null && form.getId().equals(emp.getId()))
+//						&& form.getPass().equals(emp.getPass());
+//				if (isLogin) {
+//					//ログイン情報をセッションに保存
+//					session.setAttribute("user", emp);
+//					mav.setViewName("redirect:/display");
+//				} else {
+//					mav.setViewName("login/login");
+//					mav.addObject("msg", "ログインに失敗しました");
+//				}
+//
+//			}
+//
+//
+//		return mav;
+//
+//	}
+//
+//}
