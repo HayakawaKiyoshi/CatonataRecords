d42cbf2a60b55e2ec3285bacd9ca2d098389cc24
akutsu <akutsu@EGRID-PC30.egrid.co.jp>
2022-06-23 12:36:29 +0900

    CommonDaoの変更
    阿久津 2022/6/23

--- CatonataRecords/src/main/java/com/catonata/dao/CommonDao.java
+++ CatonataRecords/src/main/java/com/catonata/dao/CommonDao.java
@@ -4,9 +4,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
 
 import com.catonata.bean.UserInformationBean;
+
 
 public class CommonDao {
 
@@ -16,7 +16,6 @@ public class CommonDao {
 		ResultSet rs = null;
 		//DBManagerをインスタンス化
 		DBManager manager = new DBManager();
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		final String LOGIN_SQL = "SELECT * FROM USER_TABLE WHERE user_name = ?";
 
 		UserInformationBean user = null;
@@ -34,7 +33,7 @@ public class CommonDao {
 				String username = rs.getString("USER_NAME");
 				String age = rs.getString("AGE");
 				String gender = rs.getString("GENDER");
-				String birthday = sdf.format(rs.getDate("BIRTHDAY"));
+				String birthday = rs.getString("BIRTHDAY");
 				String address = rs.getString("ADDRESS");
 				String email = rs.getString("EMAIL");
 				String authority = rs.getString("AUTHORITY");
@@ -65,5 +64,61 @@ public class CommonDao {
 		return user;
 
 	}
+
+//	public static List<ProductBean> findAll() {
+//
+//		Connection conn = null;
+//		PreparedStatement ps = null;
+//		ResultSet rs = null;
+//		DBManager manager = new DBManager();
+//		SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日");
+//		Sql sql = new Sql();
+//
+//		//ArrayListを初期化
+//		ArrayList<ProductBean> empList = new ArrayList<ProductBean>();
+//
+//		try {
+//			conn = manager.getConn();
+//
+//			ps = conn.prepareStatement(sql.getALLDISPSQL());
+//			rs = ps.executeQuery();
+//
+//			//結果をuserインスタンスに設定し、
+//			//ArrayListインスタンスに追加
+//			while (rs.next()) {
+//				String empid = rs.getString("emp_id");
+//				String pass = rs.getString("emp_pass");
+//				String empname = rs.getString("emp_name");
+//				String gender;
+//				if (rs.getString("gender").equals("1")) {
+//					gender = "男";
+//				} else {
+//					gender = "女";
+//				}
+//				String address = rs.getString("adress");
+//				String birthday = sdf.format(rs.getDate("birthday"));
+//				String authority;
+//				if (rs.getString("authority").equals("1")) {
+//					authority = "管理者";
+//				} else {
+//					authority = "一般";
+//				}
+//				String deptname = rs.getString("dept_name");
+//				ProductBean user = new ProductBean(empid, pass, empname, gender, address,
+//						birthday, authority, deptname);
+//				empList.add(user);
+//			}
+//		} catch (ClassNotFoundException e) {
+//			e.printStackTrace();
+//
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//			return null;
+//		}finally {
+//			manager.close(conn);
+//		}
+//		return empList;
+//
+//	}
 
 }
