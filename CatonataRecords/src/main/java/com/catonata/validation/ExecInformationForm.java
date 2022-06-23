05ad81cbe98ab824c3cb9c10978c5272fa43496a
伊藤 馨 < @192.168.100.7>
2022-06-23 10:07:19 +0900

    ExecInformationForm
    伊藤 6/23
    中身更新

--- CatonataRecords/src/main/java/com/catonata/validation/ExecInformationForm.java
+++ CatonataRecords/src/main/java/com/catonata/validation/ExecInformationForm.java
@@ -2,4 +2,28 @@ package com.catonata.validation;
 
 public class ExecInformationForm {
 
+	private String name;
+	private String password;
+	private String label;
+	private String mail;
+	private String address;
+	private String banknumber;
+	private String bankname;
+
+	public String getName() {return name;}
+	public void setName(String name) {this.name = name;}
+	public String getPassword() {return password;}
+	public void setPassword(String password) {this.password = password;}
+	public String getLabel() {return label;}
+	public void setLabel(String label) {this.label = label;}
+	public String getMail() {return mail;}
+	public void setMail(String mail) {this.mail = mail;}
+	public String getAddress() {return address;}
+	public void setAddress(String address) {this.address = address;}
+	public String getBanknumber() {return banknumber;}
+	public void setBanknumber(String banknumber) {this.banknumber = banknumber;}
+	public String getBankname() {return bankname;}
+	public void setBankname(String bankname) {this.bankname = bankname;}
+
+
 }
