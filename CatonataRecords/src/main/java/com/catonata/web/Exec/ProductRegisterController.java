338e093d0ee4c3cde0e368b865cfaa822c80c65f
akutsu <akutsu@EGRID-PC30.egrid.co.jp>
2022-06-23 14:33:04 +0900

    productregisterControllerの変更
    阿久津 2022/6/23

--- CatonataRecords/src/main/java/com/catonata/web/Exec/ProductRegisterController.java
+++ CatonataRecords/src/main/java/com/catonata/web/Exec/ProductRegisterController.java
@@ -1,5 +1,43 @@
 package com.catonata.web.Exec;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.catonata.validation.ProductForm;
+
+
+@Controller
+@RequestMapping("/exec")
 public class ProductRegisterController {
+	@Autowired
+	HttpSession session;
+
+	@RequestMapping(path = "/register", method = RequestMethod.POST)
+	public ModelAndView index2(@Validated() ProductForm form,BindingResult result, ModelAndView mav) {
+//		InsertForm user = (InsertForm) session.getAttribute("user");
+//		mav.addObject("user", user);
+
+		//バリデーションエラーの確認
+		if (result.hasErrors()) {
+			mav.setViewName("exec/register/ProductRegister");
+
+		} else {
+
+			//入力フォームをセッションに保存
+			session.setAttribute("proForm", form);
+			mav.setViewName("insert/insertCheck");
+		}
+
+		return mav;
+
+	}
+
 
 }
