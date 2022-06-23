a3105a39998e96589cfaa23c1987ad7cf8cc2b80
伊藤 馨 < @192.168.100.7>
2022-06-23 09:37:12 +0900

    ExecInformationBean
    伊藤 2022/6/22
    Beanの中身を記述

--- CatonataRecords/src/main/java/com/catonata/bean/ExecInformationBean.java
+++ CatonataRecords/src/main/java/com/catonata/bean/ExecInformationBean.java
@@ -2,4 +2,27 @@ package com.catonata.bean;
 
 public class ExecInformationBean {
 
+	private String label;
+	private String password;
+	private String mail;
+	private String address;
+	private String banknumber;
+	private String bankname;
+	private String authority;
+
+	public String getLabel() {return label;}
+	public void setLabel(String label) {this.label = label;}
+	public String getPassword() {return password;}
+	public void setPassword(String password) {this.password = password;}
+	public String getMail() {return mail;}
+	public void setMail(String mail) {this.mail = mail;}
+	public String getAddress() {return address;}
+	public void setAddress(String address) {this.address = address;}
+	public String getBanknumber() {return banknumber;}
+	public void setBanknumber(String banknumber) {this.banknumber = banknumber;}
+	public String getBankname() {return bankname;}
+	public void setBankname(String bankname) {this.bankname = bankname;}
+	public String getAuthority() {return authority;}
+	public void setAuthority(String authority) {this.authority = authority;}
+
 }
