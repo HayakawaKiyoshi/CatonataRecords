1dca8c13f6dbd911197dabfc83dd7f3224f0494b
akutsu <akutsu@EGRID-PC30.egrid.co.jp>
2022-06-24 12:02:13 +0900

    exectopcontrollerの変更
    阿久津 2022/6/24

--- CatonataRecords/src/main/java/com/catonata/web/Exec/ExecTopController.java
+++ CatonataRecords/src/main/java/com/catonata/web/Exec/ExecTopController.java
@@ -1,5 +1,62 @@
 package com.catonata.web.Exec;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.catonata.bean.ProductBean;
+import com.catonata.bean.UserInformationBean;
+import com.catonata.dao.ExecDao;
+
+@Controller
+@RequestMapping("/exec")
 public class ExecTopController {
 
+	@Autowired
+	HttpSession session;
+
+	@RequestMapping("/logintop")
+	public String top(Model model) {
+
+	//全件表示のDaoを呼び出すメソッド
+			List<ProductBean> empList = ExecDao.findAll();
+
+			//全件をセッションに保存
+			session.setAttribute("emplist", empList);
+			UserInformationBean user = (UserInformationBean) session.getAttribute("LoginUser");
+
+			//ログインしているかどうかの確認
+			if (user != null) {
+				model.addAttribute("emplist", empList);
+
+			} else {
+				return "redirect:/spring/login";
+			}
+				return "exec/mypage/UserTop";
+	}
+
+	@RequestMapping("/top")
+	public String disp(Model model) {
+
+	//全件表示のDaoを呼び出すメソッド
+			List<ProductBean> empList = ExecDao.findAll();
+
+			//全件をセッションに保存
+			session.setAttribute("emplist", empList);
+//			InsertForm user = (InsertForm) session.getAttribute("LoginUser");
+
+			//ログインしているかどうかの確認
+//			if (user != null) {
+				model.addAttribute("emplist", empList);
+//
+//			} else {
+//				return "redirect:/spring/login";
+//			}
+				return "exec/mypage/UserTop";
+	}
 }
