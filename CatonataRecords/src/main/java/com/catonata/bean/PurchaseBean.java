51c97d0eac94ade2947a108bfa03de32b0174fda
akutsu <akutsu@EGRID-PC30.egrid.co.jp>
2022-06-25 16:18:07 +0900

    購入履歴変更
    阿久津 2022/6/25

--- CatonataRecords/src/main/java/com/catonata/bean/PurchaseBean.java
+++ CatonataRecords/src/main/java/com/catonata/bean/PurchaseBean.java
@@ -3,6 +3,9 @@ package com.catonata.bean;
 public class PurchaseBean {
 
 	private String id;
+	private String pro_name;
+	private String artist;
+	private String price;
 	private String label;
 	private String purchase_date;
 	private String delivery_date;
@@ -15,5 +18,23 @@ public class PurchaseBean {
 	public void setPurchase_date(String purchase_date) {this.purchase_date = purchase_date;}
 	public String getDelivery_date() {return delivery_date;}
 	public void setDelivery_date(String delivery_date) {this.delivery_date = delivery_date;}
+	public String getPro_name() {
+		return pro_name;
+	}
+	public void setPro_name(String pro_name) {
+		this.pro_name = pro_name;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getArtist() {
+		return artist;
+	}
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
 
 }
