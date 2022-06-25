c611e90459fd81328df3bca5a6c6e1a9496a5b76
akutsu <akutsu@EGRID-PC30.egrid.co.jp>
2022-06-25 11:23:43 +0900

    TopPageControllerの変更
    阿久津 2022/6/25

--- CatonataRecords/src/main/java/com/catonata/web/Common/TopPageController.java
+++ CatonataRecords/src/main/java/com/catonata/web/Common/TopPageController.java
@@ -6,21 +6,42 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.catonata.bean.ProductBean;
 import com.catonata.dao.CommonDao;
 
 @Controller
+@RequestMapping(path="/top")
 public class TopPageController {
 	@Autowired
 	HttpSession session;
 
 	@RequestMapping(path="/TopPage")
-	private String toppage () {
+	private ModelAndView  toppage (ModelAndView mav) {
 		//表示するものは商品全件表示
+		mav.setViewName("sitetop/SiteTop");
 		List<ProductBean> ap = CommonDao.allProduct();
 		session.setAttribute("allproduct", ap);
-		return "sitetop/SiteTop";
+		mav.addObject("allproduct",ap);
+		return mav;
 	}
+
+	@PostMapping("/search")
+	public ModelAndView search(@RequestParam("msg") String msg,ModelAndView mav) {
+
+
+		System.out.println(msg);
+
+		List<ProductBean> proname = CommonDao.proSearch(msg);
+
+		mav.addObject("allproduct",proname);
+		mav.setViewName("sitetop/SiteTop");
+
+		return mav;
+	}
+
 }
