03fada05990c9dd3c2f8b2cb45a32f3d1efbe135
akutsu <akutsu@EGRID-PC30.egrid.co.jp>
2022-06-25 17:05:41 +0900

    mypageの変更
    阿久津 2022/6/25

--- CatonataRecords/src/main/java/com/catonata/web/General/MyPageUpdateDeleteController.java
+++ CatonataRecords/src/main/java/com/catonata/web/General/MyPageUpdateDeleteController.java
@@ -1,5 +1,10 @@
 package com.catonata.web.General;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mypageUpdate")
 public class MyPageUpdateDeleteController {
 
 }
