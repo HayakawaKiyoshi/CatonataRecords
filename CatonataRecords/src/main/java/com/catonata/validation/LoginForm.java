8a64beda14b4fa29fc0c14c74e5b8bff0a8394ad
伊藤 馨 < @192.168.100.7>
2022-06-27 11:57:41 +0900

    中身更新
    伊藤 6/27
    バリデーションbyte数チェックを追加

--- CatonataRecords/src/main/java/com/catonata/validation/LoginForm.java
+++ CatonataRecords/src/main/java/com/catonata/validation/LoginForm.java
@@ -2,10 +2,14 @@ package com.catonata.validation;
 
 import org.hibernate.validator.constraints.NotEmpty;
 
+import com.catonata.validation.ByteCheckValidation.ByteCheck;
+
 public class LoginForm {
 	@NotEmpty
+	@ByteCheck(charset="UTF-8",min=1, max=60)
 	private String name;
 	@NotEmpty
+	@ByteCheck(charset="UTF-8",min=1, max=20)
 	private String password;
 
 	public String getName() {
