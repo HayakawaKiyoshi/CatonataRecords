c48a126891eba8f26e61ce44e14d4902745818a4
akutsu <akutsu@EGRID-PC30.egrid.co.jp>
2022-06-27 16:05:44 +0900

    UserInformationFormの変更
    阿久津 2022/6/27
    validationの変更

--- CatonataRecords/src/main/java/com/catonata/validation/UserInformationForm.java
+++ CatonataRecords/src/main/java/com/catonata/validation/UserInformationForm.java
@@ -2,23 +2,31 @@ package com.catonata.validation;
 
 import org.hibernate.validator.constraints.NotEmpty;
 
+import com.catonata.validation.ByteCheckValidation.ByteCheck;
+
 public class UserInformationForm {
 
 
 	private String id;
 	@NotEmpty
+	@ByteCheck(charset="UTF-8",min=1, max=20)
 	private String password;
 	@NotEmpty
+	@ByteCheck(charset="UTF-8",min=1, max=60)
 	private String name;
 	@NotEmpty
+	@ByteCheck(charset="UTF-8",min=1, max=3)
 	private String age;
 	@NotEmpty
+	@ByteCheck(charset="UTF-8",min=1, max=1)
 	private String gender;
 	@NotEmpty
 	private String birthday;
 	@NotEmpty
+	@ByteCheck(charset="UTF-8",min=1, max=60)
 	private String address;
 	@NotEmpty
+	@ByteCheck(charset="UTF-8",min=1, max=50)
 	private String email;
 
 	private String authority;
