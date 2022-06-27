14bd09c3904e8e96a5763743962401a6e0db5161
伊藤 馨 < @192.168.100.7>
2022-06-27 14:41:16 +0900

    中身更新
    伊藤 6/27
    新規登録コントローラー中身更新

--- CatonataRecords/src/main/java/com/catonata/web/Common/SignUpController.java
+++ CatonataRecords/src/main/java/com/catonata/web/Common/SignUpController.java
@@ -68,7 +68,7 @@ public class SignUpController {
 		if (result.hasErrors()) {
 			return "/newregister/CardRegister";
 		}
-		UserInformationBean uif = (UserInformationBean)session.getAttribute("uif");
+		UserInformationForm uif = (UserInformationForm)session.getAttribute("uif");
 		session.setAttribute("uif", uif);
 		session.setAttribute("ccif", ccif);
 		//基本情報の入力が正常であれば、登録内容確認画面へ
