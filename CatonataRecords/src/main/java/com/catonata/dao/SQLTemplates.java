7e4a1d6063b27aee0d027c64cb49a3bbdb379719
伊藤 馨 < @192.168.100.7>
2022-06-27 12:02:44 +0900

    test

--- CatonataRecords/src/main/java/com/catonata/dao/SQLTemplates.java
+++ CatonataRecords/src/main/java/com/catonata/dao/SQLTemplates.java
@@ -2,6 +2,17 @@ package com.catonata.dao;
 
 public class SQLTemplates {
 
-
+	/**
+	 * 参考用
+	 */
+	private final String SEARCH_SQL =
+			"SELECT E.emp_id, E.emp_pass, E.emp_name,"
+	+ " (CASE WHEN E.gender = 1 THEN '男性' WHEN E.gender = 2 THEN '女性' END) AS \"gender1\","
+	+ " E.address, TO_CHAR(E.birthday, 'YYYY-MM-DD') AS \"birth\","
+	+ " (CASE WHEN E.authority = 1 THEN '一般' WHEN E.authority = 2 THEN '管理者' END) AS \"autho\","
+	+ " E.dept_id, D.dept_name"
+	+ " FROM emp_table E INNER JOIN dept_table D"
+	+ " ON E.dept_id = D.dept_id"
+	+ " WHERE emp_id = ? AND emp_pass = ?";
 
 }
